ae3bc4f9e076d81f33a6f1bf4573634149e99ab0
Patrixsbs <Patrixsbs@d47a0d4e-9465-2e3f-8376-a17037815100>
2011-09-25 23:45:29 +0000

    Adicionando arquivo de configuração para dar suporte ao MySql ao sistema

---
+++ kap/models/mysql.php
@@ -0,0 +1,8 @@
+<?php
+$this->_dnstype=1;
+$this->_dsn='mysql:host=localhost;dbname=kap';
+$this->_driver='mysql';
+$this->_user='kap';
+$this->_pass='kap';
+//$this->_dnstype=2;
+//$this->_dsn='mysql://USUARIO:SENHA@SERVIDOR/BASE';
